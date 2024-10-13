cea2bf1d095931a9a6d7944124e26e58fe9ba579
Fury Shop <95311122+FuryShop@users.noreply.github.com>
2024-10-13 16:58:08 +0200

    Add files via upload

---
+++ fury_stll/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game { 'gta5' }
+author 'Fury Shop'
+description 'Moonshinestills'
+version '1.0.0'
+
+
+data_file 'DLC_ITYP_REQUEST' 'fury_propstill_01'
+
+
+
+---[[ Licence
+---fury_still © 2024 by Fury is licensed under CC BY-NC-SA 4.0 
+---If you want to use this in one of youre free scripts please make a attribution. Not allowed to use in any paid scripts
+---only non-commercial purposes.
+---]]
